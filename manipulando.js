cce8608044b2b575b1eab342fb1640e10b7dd0ce
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-17 15:46:45 -0300

    Create manipulando.js

---
+++ manipulando.js
@@ -0,0 +1,18 @@
+//Manipulando Strings e Números
+
+//Transformando letras minúsculas em maiúsculas e vice e versa
+
+//toUpperCase() or toLowerCase()
+// let word = 'Programar é muito bacana!'
+// console.log(word.toLowerCase())
+
+//Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
+
+// let phrase = 'Eu quero viver o amor!'
+// let myArray = phrase.split(' ')
+// let outraVariável = myArray.join('-')
+// console.log(outraVariável)
+
+//Verificar se o texto contém a palavra Amor
+
+let phrase = 'Eu quero viver !'
