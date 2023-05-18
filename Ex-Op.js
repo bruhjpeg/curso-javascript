d8275fa186b9d7180402ea9af5369cd849e2345f
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-18 17:44:51 -0300

    Update Ex-Op.js

--- Ex-Op.js
+++ Ex-Op.js
@@ -1,1 +1,39 @@
 // Expressions and Operators
+
+//let number
+
+// já é uma expressão
+
+let number = 1
+
+//toda expressão no javascript pode ou não terminar com ponto e vírgula.
+// no javascript ela é facultativa, algumas linguagens elas são obrigatórias. 
+
+//------------------------------------------
+//operador binário:
+
+//precisa de dois números para que o operador esteja entre eles.
+
+//exemplo:
+
+// console.log(number + 1)
+//------------------------------------
+//operador unário
+
+// que só tem um elemento
+//operador na frente para incrementar ele ou decrementar
+
+//ex:
+
+// console.log(++number)
+//console.log(-- number)
+
+//--------------------------------------
+//operador ternário
+
+//vai receber uma expressão
+
+//ex:
+//único caso de operador ternário:
+// console.log(false ? 'alo' : 'nada')
+
