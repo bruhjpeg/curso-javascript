bbdc7b4da238e9e287b8438273bb4fb4a452fbf1
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-19 11:00:48 -0300

    .

--- Ex-Op.js
+++ Ex-Op.js
@@ -4,7 +4,7 @@
 
 // já é uma expressão
 
-let number = 1
+// let number = 1
 
 //toda expressão no javascript pode ou não terminar com ponto e vírgula.
 // no javascript ela é facultativa, algumas linguagens elas são obrigatórias. 
@@ -36,4 +36,16 @@ let number = 1
 //ex:
 //único caso de operador ternário:
 // console.log(false ? 'alo' : 'nada')
+//--------------------------------------------
 
+// new
+// criar um novo objeto
+
+// ex
+
+let name = new String('Bruna')
+name.surName = 'Assis'
+let age = new Number(23)
+console.log(name, age)
+
+//  ou seja o new, transformou o name e o age em um objeto.
