7516bf019f197ebd0137c0d22d86ccfb06bb2090
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-24 16:07:04 -0300

    Create if-else.js

---
+++ if-else.js
@@ -0,0 +1,12 @@
+let temperature = 40
+
+
+if(temperature >= 37.5) {
+    console.log('Está com febre alta')
+} else if(temperature >= 37) {
+    console.log('Está com febre moderada')
+} else if (temperature <=30){
+   console.log('Procure um médico') 
+} else {
+    console.log('Está saudável')
+}
