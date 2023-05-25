0fe59dc342452facdebe6417f275be254a23a58b
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-25 15:16:36 -0300

    .

--- Estruturas-de-repetição.js
+++ Estruturas-de-repetição.js
@@ -1,3 +1,32 @@
-for(let age = 20; age > 0; age--){
-    console.log(age)
+// for (let age = 10; age > 0; age--) {
+//   if (age === 5) {
+//     break;
+//     continue
+//   }
+//   console.log(age);
+// }
+
+// let age = 37373484387;
+// while (age >= 10) {
+//   console.log(age);
+//   age /= 50;
+// }
+
+// let name = "Bruna";
+// let names = ["Camille, Luciane, Luciano"];
+
+// for (let nomes of names) {
+//     console.log(nomes)
+// }
+
+let person = {
+  name: "Bruna",
+  age: 23,
+  profission: "Programmer",
+  live: "Brazil",
+};
+
+for (let pessoa in person) {
+  console.log(pessoa);
+  console.log(person[pessoa])
 }
---
+++ node_ea6962010c2e4.tmp.js
@@ -0,0 +1,13 @@
+// for (let age = 10; age > 0; age--) {
+//   if (age === 5) {
+//     break;
+//     continue
+//   }
+//   console.log(age);
+// }
+
+let age = 0
+while(age<10) {
+    console.log(age)
+    age--
+}
