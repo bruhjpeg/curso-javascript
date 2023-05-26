815ac2b896a5c00cd4af58d7c62a6467af9feccb
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-26 12:06:57 -0300

    .

--- stage4/media-aluno.js
+++ stage4/media-aluno.js
@@ -1,15 +1,27 @@
-alert("Olá, bem vindo(a) ao nosso sistema para cálculo de médiasn bimestrais.");
+alert("Olá, bem vindo(a) ao nosso sistema para cálculo de médias bimestrais.");
 
 let aluno = prompt("Digite seu nome completo");
-let nota1 =prompt("Digite a primeira nota do seu bimestre");
-let nota2 =prompt("Digite a segunda nota do seu bimestre");
-let nota3 =prompt("Digite a terceira nota do seu bimestre");
+let nota1 = prompt("Digite a primeira nota do seu bimestre");
+let nota2 = prompt("Digite a segunda nota do seu bimestre");
+let nota3 = prompt("Digite a terceira nota do seu bimestre");
 
 let media = Number((nota1 + nota2 + nota3) / 3);
 
+calculate();
 
-calculate()
-
-function calculate(){
-    if()
+function calculate() {
+  if (media >= 6) {
+    alert(`${aprovado}`);
+  } else {
+    alert(`${reprovado}`);
+  }
+  return calculate;
 }
+
+const aprovado = alert(
+  `Parabéns ${aluno}! sua média é: ${media} . E você está aprovado.`
+);
+
+const reprovado = alert(
+  `Infelizmente ${aluno} foi reprovado(a) pois sua média foi ${media}. Boa recuperação!`
+);
