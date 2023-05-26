95772cc66ad770d6e68974b358bab38ea48e8613
BrunaAquino22 <brunahasting99@gmail.com>
2023-05-26 08:47:20 -0300

    .

---
+++ stage4/f158.js
@@ -0,0 +1,12 @@
+alert("Bem vindo(a) a sua calculadora de soma de números!");
+
+let n1 = Number(prompt("Digite um número"));
+let n2 = Number(prompt("Digite outro número"));
+
+let resultado = Number(n1 + n2);
+
+alert(`A soma de ${n1} com ${n2} é: ${resultado}`);
+
+let avaliaçao = prompt("Diga-nos se ficou satisfeito com nossa aplicação:");
+
+console.log(avaliaçao);
