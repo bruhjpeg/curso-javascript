b46718f21bb71e936919ffe090d2054c836a19bc
BrunaAquino22 <brunahasting99@gmail.com>
2023-06-07 14:58:19 -0300

    Update projetoIMC.js

--- projetos/projetoIMC.js
+++ projetos/projetoIMC.js
@@ -1,2 +1,14 @@
 // variables
 
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("height");
+
+// função criada e atribuída como valor:
+// só vai ser executada quando eu clicar no botão de submit.
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+};
