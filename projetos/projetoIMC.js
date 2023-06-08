1b8bbd70cae570a6b7d957daa6bcc1408185026d
BrunaAquino22 <brunahasting99@gmail.com>
2023-06-08 17:04:17 -0300

    Update projetoIMC.js

--- projetos/projetoIMC.js
+++ projetos/projetoIMC.js
@@ -4,9 +4,26 @@ const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
-const modalWrapper = document.querySelector(".modal-wrapper");
-const modalMessage = document.querySelector(".modal .title span");
-const modalBtnClose = document.querySelector(".modal .close");
+// variáveis que excluimos.
+// const modalWrapper = document.querySelector(".modal-wrapper");
+// const modalMessage = document.querySelector(".modal .title span");
+// const modalBtnClose = document.querySelector(".modal .close");
+
+// Estrutura de dados com as responsabilidades separadas.
+// objeto que vai orquestrar:
+
+const Modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  buttonClose: document.querySelector(".modal .close"),
+
+  open() {
+    Modal.wrapper.classList.add("open");
+  },
+  close() {
+    Modal.wrapper.classList.remove("open");
+  },
+};
 
 // função criada e atribuída como valor:
 // só vai ser executada quando eu clicar no botão de submit.
@@ -19,15 +36,14 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
 
-  modalMessage.innerText = message;
-
-  modalWrapper.classList.add("open");
+  Modal.message.innerText = message;
+  Modal.open();
 };
 
 // evento para quando clicar para fechar o popup
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove("open");
+Modal.buttonClose.onclick = () => {
+  Modal.close();
 };
 
 // Cálculo do IMC
