25a408881e09a74d06b677adc3ca1cde9319c991
BrunaAquino22 <brunahasting99@gmail.com>
2023-06-12 15:00:26 -0300

    .

--- projetos/projetoIMC.js
+++ projetos/projetoIMC.js
@@ -1,32 +1,9 @@
-// variables
+import { Modal } from "../projetos/modal.js";
 
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
-// variáveis que excluimos.
-// const modalWrapper = document.querySelector(".modal-wrapper");
-// const modalMessage = document.querySelector(".modal .title span");
-// const modalBtnClose = document.querySelector(".modal .close");
-
-// Estrutura de dados com as responsabilidades separadas.
-// objeto que vai orquestrar:
-
-const Modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title span"),
-  buttonClose: document.querySelector(".modal .close"),
-
-  open() {
-    Modal.wrapper.classList.add("open");
-  },
-  close() {
-    Modal.wrapper.classList.remove("open");
-  },
-};
-
-// função criada e atribuída como valor:
-// só vai ser executada quando eu clicar no botão de submit.
 form.onsubmit = (event) => {
   event.preventDefault();
 
@@ -40,13 +17,6 @@ form.onsubmit = (event) => {
   Modal.open();
 };
 
-// evento para quando clicar para fechar o popup
-
-Modal.buttonClose.onclick = () => {
-  Modal.close();
-};
-
-// Cálculo do IMC
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
 }
