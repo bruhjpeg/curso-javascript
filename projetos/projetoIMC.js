d221bca34a3ba6456e202e97a559b710f914a40f
BrunaAquino22 <brunahasting99@gmail.com>
2023-06-13 14:01:33 -0300

    .

--- projetos/projetoIMC.js
+++ projetos/projetoIMC.js
@@ -1,4 +1,5 @@
 import { Modal } from "../projetos/modal.js";
+import { alertError } from "../projetos/alert-error.js";
 
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
@@ -9,6 +10,14 @@ form.onsubmit = (event) => {
 
   const weight = inputWeight.value;
   const height = inputHeight.value;
+  const showAlertError = notANumber(weight) || notANumber(height);
+
+  if (showAlertError) {
+   alertError.open()
+    return;
+  }
+
+  alertError.close()
 
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
@@ -16,6 +25,10 @@ form.onsubmit = (event) => {
   Modal.message.innerText = message;
   Modal.open();
 };
+
+function notANumber(value) {
+  return isNaN(value) || value == "";
+}
 
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
